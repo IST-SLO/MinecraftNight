6aed317240c6e02e1219146e0016e20bbc69914c
IST-SLO <61441622+IST-SLO@users.noreply.github.com>
2020-09-04 18:57:42 -0400

    Create Windows.bat

---
+++ Windows.bat
@@ -0,0 +1,3 @@
+echo Ask the eBoard member for the password!
+echo If this doesnt work, its possible that the port needs to be updated. Ask the eboard
+ssh member@0.tcp.ngrok.io -L 25565:localhost:25565 -p 18069 -N
